e8754f3cd4da88f9c365e19e1084ae9f685e68dd
Luiza0607 <110295222+Luiza0607@users.noreply.github.com>
2023-09-10 18:40:12 +0200

    Adding structure project and images

---
+++ script.js
@@ -0,0 +1,6 @@
+const open = document.querySelector('.shopping');
+const close = document.querySelector('.close');
+const list = document.querySelector('.list');
+const listCard = document.querySelector('.listCard');
+const total = document.querySelector('.total');
+const quantity = document.querySelector('.quantity');
